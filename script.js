c4811c1ccd5ccc52babf98be8899128e33183aa8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-12 22:42:43 +0530

    add: metricData function to files metric data

--- script.js
+++ script.js
@@ -5,5 +5,31 @@ async function getData(q) {
   return data;
 }
 
+function metricData(data) {
+  return {
+    name: data.location.name,
+    region: data.location.region,
+    country: data.location.country,
+    localtime: data.location.localtime,
+
+    temp: data.current.temp_c + '°C',
+    is_day: data.current.is_day ? 'Day' : 'Night',
+    condition_text: data.current.condition.text,
+    condition_icon: data.current.condition.icon.slice(2),
+
+    wind_speed: data.current.wind_kph + 'kph',
+    wind_dir: data.current.wind_dir,
+    wind_deg: data.current.wind_degree + '°',
+
+    pressure: data.current.pressure_mb + 'mb',
+    precip: data.current.precip_mm + 'mm',
+
+    humidity: data.current.humidity + '%',
+    could_cover: data.current.cloud + '%',
+    feels_like: data.current.feelslike_c + '°C',
+    visibility: data.current.vis_km + 'km',
+    uv: data.current.uv,
+  };
+}
 
 
