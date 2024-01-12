43f879f15432b44f4e707b4328d1d44b78d5e1e5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-12 22:42:02 +0530

    add: API handler function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Testing');
+async function getData(q) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=cc573d36cef54f8fa6524319241201&q=${q}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+
+
