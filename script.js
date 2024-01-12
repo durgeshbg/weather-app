cdf885540b5071eada40d293f87f59ea9932efab
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-12 22:43:13 +0530

    add: imperialData function to filter imperial data

--- script.js
+++ script.js
@@ -31,5 +31,31 @@ function metricData(data) {
     uv: data.current.uv,
   };
 }
+function imperialData(data) {
+  return {
+    name: data.location.name,
+    region: data.location.region,
+    country: data.location.country,
+    localtime: data.location.localtime,
+
+    temp: data.current.temp_f + '°F',
+    is_day: data.current.is_day ? 'Day' : 'Night',
+    condition_text: data.current.condition.text,
+    condition_icon: data.current.condition.icon.slice(2),
+
+    wind_speed: data.current.wind_mph + 'mph',
+    wind_dir: data.current.wind_dir,
+    wind_deg: data.current.wind_degree + '°',
+
+    pressure: data.current.pressure_in + 'in',
+    precip: data.current.precip_in + 'in',
+
+    humidity: data.current.humidity + '%',
+    could_cover: data.current.cloud + '%',
+    feels_like: data.current.feelslike_f + '°F',
+    visibility: data.current.vis_miles + 'miles',
+    uv: data.current.uv,
+  };
+}
 
 
