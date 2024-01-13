0e09b84d9fc32afc51500b567c0724dba3f3f0f9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-13 09:30:34 +0530

    add: merge imperialData and metricData functions

--- script.js
+++ script.js
@@ -5,68 +5,50 @@ async function getData(q) {
   return data;
 }
 
-function metricData(data) {
-  return {
+function metricData(data, imperial = false) {
+  const commonData = {
     name: data.location.name,
     region: data.location.region,
     country: data.location.country,
     localtime: data.location.localtime,
-
-    temp: data.current.temp_c + '°C',
     is_day: data.current.is_day ? 'Day' : 'Night',
     condition_text: data.current.condition.text,
     condition_icon: data.current.condition.icon.slice(2),
-
-    wind_speed: data.current.wind_kph + 'kph',
     wind_dir: data.current.wind_dir,
     wind_deg: data.current.wind_degree + '°',
-
+    humidity: data.current.humidity + '%',
+    could_cover: data.current.cloud + '%',
+    uv: data.current.uv,
+  };
+  const m = {
+    ...commonData,
+    temp: data.current.temp_c + '°C',
+    wind_speed: data.current.wind_kph + 'kph',
     pressure: data.current.pressure_mb + 'mb',
     precip: data.current.precip_mm + 'mm',
-
-    humidity: data.current.humidity + '%',
-    could_cover: data.current.cloud + '%',
     feels_like: data.current.feelslike_c + '°C',
     visibility: data.current.vis_km + 'km',
-    uv: data.current.uv,
   };
-}
-function imperialData(data) {
-  return {
-    name: data.location.name,
-    region: data.location.region,
-    country: data.location.country,
-    localtime: data.location.localtime,
-
+  const i = {
+    ...commonData,
     temp: data.current.temp_f + '°F',
-    is_day: data.current.is_day ? 'Day' : 'Night',
-    condition_text: data.current.condition.text,
-    condition_icon: data.current.condition.icon.slice(2),
-
     wind_speed: data.current.wind_mph + 'mph',
-    wind_dir: data.current.wind_dir,
-    wind_deg: data.current.wind_degree + '°',
-
     pressure: data.current.pressure_in + 'in',
     precip: data.current.precip_in + 'in',
-
-    humidity: data.current.humidity + '%',
-    could_cover: data.current.cloud + '%',
     feels_like: data.current.feelslike_f + '°F',
     visibility: data.current.vis_miles + 'miles',
-    uv: data.current.uv,
   };
+  return imperial ? i : m;
 }
 
 async function handleSubmit(e) {
   const q = document.querySelector('input').value;
-  console.log(q);
   const rawData = await getData(q);
   if (rawData.error) {
     console.log(rawData.error.message);
   } else {
-    const processedData = imperialData(rawData);
-    console.log(rawData);
+    const processedData = metricData(rawData, true);
+    console.log(processedData);
   }
   e.target.reset();
   e.preventDefault();
