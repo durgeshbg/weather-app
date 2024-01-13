3d56c1b6b5067ce59ea2cef8a278462732ddeb71
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-13 10:15:18 +0530

    add: rawData -> global scope & error handleing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let rawData = {};
+
 async function getData(q) {
   const url = `https://api.weatherapi.com/v1/current.json?key=cc573d36cef54f8fa6524319241201&q=${q}`;
   const res = await fetch(url);
@@ -5,52 +7,58 @@ async function getData(q) {
   return data;
 }
 
-function metricData(data, imperial = false) {
+function metricData(imperial = false) {
+  if (rawData.error) {
+    return {
+      error: rawData.error.message,
+    };
+  } else if (Object.keys(rawData).length == 0) {
+    return {
+      error: 'Please select a location.',
+    };
+  }
   const commonData = {
-    name: data.location.name,
-    region: data.location.region,
-    country: data.location.country,
-    localtime: data.location.localtime,
-    is_day: data.current.is_day ? 'Day' : 'Night',
-    condition_text: data.current.condition.text,
-    condition_icon: data.current.condition.icon.slice(2),
-    wind_dir: data.current.wind_dir,
-    wind_deg: data.current.wind_degree + '°',
-    humidity: data.current.humidity + '%',
-    could_cover: data.current.cloud + '%',
-    uv: data.current.uv,
+    name: rawData.location.name,
+    region: rawData.location.region,
+    country: rawData.location.country,
+    localtime: rawData.location.localtime,
+    is_day: rawData.current.is_day ? 'Day' : 'Night',
+    condition_text: rawData.current.condition.text,
+    condition_icon: rawData.current.condition.icon.slice(2),
+    wind_dir: rawData.current.wind_dir,
+    wind_deg: rawData.current.wind_degree + '°',
+    humidity: rawData.current.humidity + '%',
+    could_cover: rawData.current.cloud + '%',
+    uv: rawData.current.uv,
   };
   const m = {
     ...commonData,
-    temp: data.current.temp_c + '°C',
-    wind_speed: data.current.wind_kph + 'kph',
-    pressure: data.current.pressure_mb + 'mb',
-    precip: data.current.precip_mm + 'mm',
-    feels_like: data.current.feelslike_c + '°C',
-    visibility: data.current.vis_km + 'km',
+    temp: rawData.current.temp_c + '°C',
+    wind_speed: rawData.current.wind_kph + 'kph',
+    pressure: rawData.current.pressure_mb + 'mb',
+    precip: rawData.current.precip_mm + 'mm',
+    feels_like: rawData.current.feelslike_c + '°C',
+    visibility: rawData.current.vis_km + 'km',
   };
   const i = {
     ...commonData,
-    temp: data.current.temp_f + '°F',
-    wind_speed: data.current.wind_mph + 'mph',
-    pressure: data.current.pressure_in + 'in',
-    precip: data.current.precip_in + 'in',
-    feels_like: data.current.feelslike_f + '°F',
-    visibility: data.current.vis_miles + 'miles',
+    temp: rawData.current.temp_f + '°F',
+    wind_speed: rawData.current.wind_mph + 'mph',
+    pressure: rawData.current.pressure_in + 'in',
+    precip: rawData.current.precip_in + 'in',
+    feels_like: rawData.current.feelslike_f + '°F',
+    visibility: rawData.current.vis_miles + 'miles',
   };
   return imperial ? i : m;
 }
 
 async function handleSubmit(e) {
   const q = document.querySelector('input').value;
-  const rawData = await getData(q);
-  if (rawData.error) {
-    console.log(rawData.error.message);
-  } else {
-    const processedData = metricData(rawData, true);
-    console.log(processedData);
-  }
+  rawData = await getData(q);
+  const processedData = metricData();
+  console.log(processedData);
   e.target.reset();
   e.preventDefault();
 }
+
 document.querySelector('form').onsubmit = handleSubmit;
